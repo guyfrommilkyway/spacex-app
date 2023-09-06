0df8fb23809e3eafff8eb7ed81f10528659c20a0
Almer Tampus <almerflorestampus@gmail.com>
2023-09-06 22:43:18 +0800

    Wrap App with Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 // import packages below
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 // import components below
 import App from './App.tsx';
+import LoadingScreen from './components/LoadingScreen/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Suspense fallback={<LoadingScreen />}>
+        <App />
+      </Suspense>
     </ChakraProvider>
   </React.StrictMode>,
 );
