d51b5c5498a16f358370f768d040da566a800c37
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 01:02:40 +0800

    Pass LoadingScreen component as props

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 // import components below
 import App from './App.tsx';
-import LoadingScreen from './components/LoadingScreen/index.tsx';
+import LoadingScreen from '@/components/LoadingScreen/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
       <Suspense fallback={<LoadingScreen />}>
-        <App />
+        <App renderLoading={<LoadingScreen />} />
       </Suspense>
     </ChakraProvider>
   </React.StrictMode>,
