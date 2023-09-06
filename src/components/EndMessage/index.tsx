8ad9f9bc7a014012d2b9979f76475b75bb7cf7cc
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 01:02:10 +0800

    Add EndMessage component

---
+++ src/components/EndMessage/index.tsx
@@ -0,0 +1,14 @@
+// import packages below
+import { Box, Text } from '@chakra-ui/react';
+
+const EndMessage = () => {
+  return (
+    <Box p={4}>
+      <Text color='blackAlpha.600' textAlign='center'>
+        No more data to load.
+      </Text>
+    </Box>
+  );
+};
+
+export default EndMessage;
