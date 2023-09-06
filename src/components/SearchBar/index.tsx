37b9181469f7cdc154ca7033a78fbf82c2e8cbf4
Almer Tampus <almerflorestampus@gmail.com>
2023-09-06 20:13:45 +0800

    Add SearchBar component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,24 @@
+import { Box, Input } from '@chakra-ui/react';
+import React from 'react';
+
+interface Props {
+  onSearch: (key: string) => void;
+}
+
+const SearchBar: React.FC<Props> = props => {
+  const { onSearch } = props;
+
+  return (
+    <Box mb={8} bg='gray.50'>
+      <Input
+        type='text'
+        placeholder='Enter keywords'
+        onChange={e => onSearch(e?.target?.value)}
+        borderRadius='0'
+        borderColor='RGBA(0, 0, 0, 0.16)'
+      />
+    </Box>
+  );
+};
+
+export default SearchBar;
